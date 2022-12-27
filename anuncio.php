1a60012407baf95e0c604e0fdcd652e84d46e7ff
branmeco <branmeco17@gmail.com>
2022-12-26 23:47:12 -0500

    Creando la pagina para mostrar la informacion de la propiedad

--- anuncio.php
+++ anuncio.php
@@ -1,45 +1,55 @@
 <?php
+
+$id = $_GET['id'];
+$id = filter_var($id, FILTER_VALIDATE_INT);
+
+if (!$id) {
+  header('Location: /');
+}
+
+//Importar la conexión
+require 'includes/config/database.php';
+$db = conectarDB();
+//Consultar
+$query = "SELECT * FROM propiedades WHERE id = ${id}";
+
+//Obtener resultado
+$resultado = mysqli_query($db, $query);
+if(!$resultado->num_rows){
+  header('Location: /');
+}
+$propiedad = mysqli_fetch_assoc($resultado);
+
 require 'includes/funciones.php';
 incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion contenido-centrado">
-  <h1>Casa en Venta frente al bosque</h1>
-  <picture>
-    <source srcset="build/img/destacada.webp" type="image/webp" />
-    <source srcset="build/img/destacada.jpg" type="image/jpeg" />
-    <img loading="lazy" src="build/img/destacada.jpg" alt="Imagen de la propiedad" />
-  </picture>
+  <h1><?php echo $propiedad['titulo']; ?></h1>
+
+  <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la propiedad" />
+
   <div class="resumen-propiedad">
-    <p class="precio">103'000.000</p>
+    <p class="precio">$<?php echo $propiedad['precio']; ?></p>
     <ul class="iconos-caracteristicas">
       <li>
         <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc" />
-        <p>3</p>
+        <p><?php echo $propiedad['wc']; ?></p>
       </li>
       <li>
         <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento" />
-        <p>3</p>
+        <p><?php echo $propiedad['estacionamiento']; ?></p>
       </li>
       <li>
         <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones" />
-        <p>4</p>
+        <p><?php echo $propiedad['habitaciones']; ?></p>
       </li>
     </ul>
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting
-      industry. Lorem Ipsum has been the industry's standard dummy text ever
-      since the 1500s, when an unknown printer took a galley of type and
-      scrambled it to make a type specimen book. It has survived not only
-      five centuries, but also the leap into electronic typesetting,
-      remaining essentially unchanged. It was popularised in the 1960s with
-      the release of Letraset sheets containing Lorem Ipsum passages, and
-      more recently with desktop publishing software like Aldus PageMaker
-      including versions of Lorem Ipsum.
-    </p>
+    <?php echo $propiedad['descripcion']; ?>
   </div>
 </main>
 
 <?php
-  incluirTemplate('footer')
+mysqli_close($db);
+incluirTemplate('footer')
 ?>

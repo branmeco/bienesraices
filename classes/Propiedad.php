7a9ccad3298c89b32c767570e7aa8438ea1a6bcd
branmeco <branmeco17@gmail.com>
2023-01-16 08:11:14 -0500

    Añadiendo validación

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -8,6 +8,9 @@ class Propiedad
     protected static $db;
     protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
 
+    //Errores
+    protected static $errores = [];
+
     public $id;
     public $titulo;
     public $precio;
@@ -45,12 +48,11 @@ class Propiedad
         $atributos = $this->sanitizarAtributos();
 
         // Insertar en la base de datos
-        $query = "INSERT INTO propiedades ( ";
-        $query .= join('. ', array_keys($atributos));
-        $query .= " ) VALUES ( ";
-        $query .= join(", ", array_values($atributos));
-        $query .= " ) ";
-        $query = " INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId ) VALUES ( '$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId' ) ";
+        $query = " INSERT INTO propiedades ( ";
+        $query .= join(', ', array_keys($atributos));
+        $query .= " ) VALUES ( '";
+        $query .= join("', '", array_values($atributos));
+        $query .= " ') ";
 
         $resultado = self::$db->query($query);
 
@@ -73,10 +75,60 @@ class Propiedad
         $atributos = $this->atributos();
         $sanitizado = [];
 
-        foreach($atributos as $key => $value){
+        foreach ($atributos as $key => $value) {
             $sanitizado[$key] = self::$db->escape_string($value);
         }
 
         return $sanitizado;
+    }
+
+    //validaciones
+    public static function getErrores()
+    {
+        return self::$errores;
+    }
+
+    public function validar()
+    {
+        if (!$this->titulo) {
+            self::$errores[] = "Debes añadir un titulo";
+        }
+
+        if (!$this->precio) {
+            self::$errores[] = 'El Precio es Obligatorio';
+        }
+
+        if (strlen($this->descripcion) < 50) {
+            self::$errores[] = 'La descripción es obligatoria y debe tener al menos 50 caracteres';
+        }
+
+        if (!$this->habitaciones) {
+            self::$errores[] = 'El Número de habitaciones es obligatorio';
+        }
+
+        if (!$this->wc) {
+            self::$errores[] = 'El Número de Baños es obligatorio';
+        }
+
+        if (!$this->estacionamiento) {
+            self::$errores[] = 'El Número de lugares de Estacionamiento es obligatorio';
+        }
+
+        if (!$this->vendedorId) {
+            self::$errores[] = 'Elige un vendedor';
+        }
+
+        // if (!$this->imagen['name'] || $this->imagen['error']) {
+        //     $errores[] = 'La Imagen es Obligatoria';
+        // }
+
+        // // Validar por tamaño (1mb máximo)
+        // $medida = 1000 * 1000;
+
+
+        // if ($this->imagen['size'] > $medida) {
+        //     $errores[] = 'La Imagen es muy pesada';
+        // }
+        return self::$errores;
     }
 }
